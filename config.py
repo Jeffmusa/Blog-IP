0983b3cfbaa2c4786477f8473c54b2f9d90b088a
Jeff Musa <jeffmusa@gmail.com>
2018-09-14 14:57:24 +0300

    application initialized

--- config.py
+++ config.py
@@ -1,1 +1,22 @@
 import os
+
+
+
+class Config:
+
+    '''
+    General configuration parent class
+    '''
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    '''
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    '''    
